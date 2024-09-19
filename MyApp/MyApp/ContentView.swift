de6cf41c1d5c5ce997f96674d40e9b08c6e0e9f3
Moritz Tucher <moritztucher@gmail.com>
2024-09-19 08:50:33 +0200

    Setup Project

---
+++ MyApp/MyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Moritz Tucher on 19.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
